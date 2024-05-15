08d8630d8a7d5aa43271276fdcc2dc31bd3fcc62
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-05-15 22:37:47 +0600

    Create Simple Top Banner Using Hero Component

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,5 +1,24 @@
+import chair from "../../../assets/images/chair.png";
+
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div className="hero">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={chair} className=" rounded-lg w-1/2 shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn bg-gradient-to-r from-primary to-secondary text-white">
+            GET STARTED
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../Banner/Banner";
+
 const Home = () => {
   return (
     <div className="mx-5">
-      <h2>This is Home</h2>
+      <Banner></Banner>
     </div>
   );
 };
