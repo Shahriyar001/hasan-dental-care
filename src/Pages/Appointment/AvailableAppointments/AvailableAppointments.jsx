6e38c4d0be9b1958797a34ff1504695fd318891b
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-05-21 22:39:00 +0600

    Appointment Options And Available Slots With Conditional Rendering

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.jsx
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.jsx
@@ -1,11 +1,28 @@
 import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import AppointmentOption from "./AppointmentOption";
 
 const AvailableAppointments = ({ selectedDate }) => {
+  const [appointmentOptions, setAppointmentOptions] = useState([]);
+
+  useEffect(() => {
+    fetch("appointmentOption.json")
+      .then((res) => res.json())
+      .then((data) => setAppointmentOptions(data));
+  }, []);
   return (
     <section className="mt-16">
       <p className="text-center text-secondary font-bold">
         Available Appointment on {format(selectedDate, "PP")}
       </p>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6">
+        {appointmentOptions.map((option) => (
+          <AppointmentOption
+            key={option._id}
+            appointmentOption={option}
+          ></AppointmentOption>
+        ))}
+      </div>
     </section>
   );
 };
