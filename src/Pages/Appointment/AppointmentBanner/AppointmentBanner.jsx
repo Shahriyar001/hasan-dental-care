355874bbf7f8bee00c91326afe03bccf65b9c75c
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-05-21 19:52:34 +0600

    Create Appointment Banner Using React Day Picker

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.jsx
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.jsx
@@ -1,23 +1,26 @@
+import { useState } from "react";
 import chair from "../../../assets/images/chair.png";
+import { DayPicker } from "react-day-picker";
+import { format } from "date-fns";
 
 const AppointmentBanner = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   return (
-    <header>
+    <header className="my-6">
       <div className="hero ">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
             src={chair}
             alt="chair"
-            className="max-w-sm rounded-lg shadow-2xl"
+            className="max-w-md ml-6 rounded-lg shadow-2xl"
           />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+          <div className="mr-6">
+            <DayPicker
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+            />
+            <p>You have selsected date: {format(selectedDate, "PP")}</p>
           </div>
         </div>
       </div>
