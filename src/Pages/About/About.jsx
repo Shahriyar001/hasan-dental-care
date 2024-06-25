b65d782404e053ab48095e5dda1260acf358fd8f
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-06-25 23:47:10 +0600

    added oral care componant

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,11 @@
 import Dentistry from "./dentistry/Dentistry";
+import OralHealth from "./oralHealth/OralHealth";
 
 const About = () => {
   return (
     <div className="ml-6">
       <Dentistry />
+      <OralHealth />
     </div>
   );
 };
---
+++ src/Pages/About/oralHealth/OralHealth.jsx
@@ -0,0 +1,42 @@
+const OralHealth = () => {
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        // style={{
+        //   backgroundImage:
+        //     "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpghttps://static.vecteezy.com/system/resources/previews/036/595/008/non_2x/ai-generated-dental-clinic-advertisment-background-with-copy-space-free-photo.jpg)",
+        // }}
+        style={{
+          backgroundImage:
+            "url(https://static.vecteezy.com/system/resources/previews/036/595/008/non_2x/ai-generated-dental-clinic-advertisment-background-with-copy-space-free-photo.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-amber-200 bg-opacity-20"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="">
+            <div className="hero min-h-screen">
+              <div className="hero-content flex-col lg:flex-row">
+                <img
+                  src="https://dtdental.wpenginepowered.com/wp-content/uploads/2016/11/img3.png"
+                  className="w-1/2 rounded-lg"
+                />
+                <div>
+                  <h1 className="text-5xl font-bold">Box Office News!</h1>
+                  <p className="py-6">
+                    Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                    assumenda excepturi exercitationem quasi. In deleniti eaque
+                    aut repudiandae et a id nisi.
+                  </p>
+                  <button className="btn btn-primary">Get Started</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OralHealth;
